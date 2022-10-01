aaa3b1727f2f9551e331dc004b45519173d0538a
pavka21 <61372453+pavka21@users.noreply.github.com>
2022-10-01 23:46:08 +0700

    ok

--- 2023demo/17.py
+++ 2023demo/17.py
@@ -1,8 +1,15 @@
 with open('17.txt') as f:
    numbers=[int(x) for x in f]
    print(numbers)
+   maxi=0
    s=[]
+   
+   for i in range(0,len(numbers)):
+      if numbers[i]%10==3:
+         maxi.append(numbers[i])
+   
    for i in range(1,len(numbers)):
-      if numbers[i]%3==0 or numbers[i-1]%3==0:
-         s.append(numbers[i]+numbers[i-1])
+      if (numbers[i]%10==3) ^ (numbers[i-1]%10==3):
+         if (numbers[i]**2+numbers[i-1]**2) >= max(maxi)**2: 
+            s.append(numbers[i]+numbers[i-1])
    print(len(s), max(s))

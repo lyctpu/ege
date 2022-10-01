7f7a9ce4d885b651a94f34cb372168c707e7f4c6
PaulSverchkov <61372453+pavka21@users.noreply.github.com>
2022-10-01 18:51:32 +0700

    Update 5.py

---
+++ 2023demo/5.py
@@ -0,0 +1,11 @@
+for i in range(1,100):
+    chislo=''
+    num=(bin(i)[2:])
+    if num.count('1')%2==0:
+        chislo='10'+num[2:]+'0'
+
+    if num.count('1')%2!=0:
+        chislo='11'+num[2:]+'1'
+    if int(chislo,2)>40:
+        print (i, int(chislo,2))
+        break

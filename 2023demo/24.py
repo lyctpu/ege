59f942c250b8d56d5725aa9b715c5c7574fea9fd
PaulSverchkov <61372453+pavka21@users.noreply.github.com>
2022-10-06 00:23:48 +0700

    Add files via upload

--- 2023demo/24.py
+++ 2023demo/24.py
@@ -1,3 +1,21 @@
 with open('24.txt') as f:
-   numbers=[int(x) for x in f]
-   print(numbers)
+   letters=f.readline()
+   print(letters[0])
+   count,maximum=0,0
+   text=''
+   delta=0
+   for i in range(0,len(letters)-1):
+
+      if (letters[i]=='C' or letters[i]=='D' or letters[i]=='F') and (letters[i+1]=='A' or letters[i+1]=='O'):
+         delta=0
+         text=text+letters[i]+letters[i+1]
+         count+=1
+         if count>maximum: 
+            maximum=count
+     
+      else: delta+=1
+      if delta==2:
+         count=0
+         text=''
+         flag=False
+print(maximum)
